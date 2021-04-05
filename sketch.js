57ec60e18ca123b85b2bb5f7220fb7585a558431
AarunyaAttri <74667923+AarunyaAttri@users.noreply.github.com>
2021-04-05 21:20:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,8 +8,6 @@ var hr_angle,mn_angle,sc_angle;
 function setup() {
   createCanvas(400,450);
   //createSprite(0, 0, 50, 50);
-
-  
 }
 
 function draw() {
@@ -64,4 +62,22 @@ function draw() {
 //  console.log(hr)
   console.log(sc_angle);
  // console.log(sc)
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=0600 && hour<=1900){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
 }
